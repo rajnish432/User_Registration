f9b012f31cbc67896ab2e28aaba664855a6fe6b3
rajnish432 <rajnish.kahar1996@gmail.com>
2020-02-14 12:57:44 +0530

    ADDED: Validation For First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
 
 echo "Welcome to User Registration."
+read -p "Enter the First Name: " firstName
+namePattern="^[A-Z]{1}[a-z]{2,}$";
+
+function validateUserDetails()
+{
+	userDetail=$1
+	pattern=$2
+	if [[ $userDetail =~ $pattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+}
+
+validateUserDetails $firstName $namePattern
