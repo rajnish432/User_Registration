3dec0b013f1a0b00eb91bb6625bbdc6a18ebc4ec
rajnish432 <rajnish.kahar1996@gmail.com>
2020-02-14 14:50:45 +0530

    ADDED: Validation For User Email

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,7 +3,9 @@
 echo "Welcome to User Registration."
 read -p "Enter the First Name: " firstName
 read -p "Enter the Last Name: " lastName
+read -p "Enter User Email-ID: " userEmail
 namePattern="^[A-Z]{1}[a-z]{2,}$";
+emailPattern="^[a-zA-Z0-9]{3,}([+|_|-|.]?[a-zA-Z0-9])?[@]{1}[.]{0}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}[.]?{1}[a-zA-Z]?{2,3}$"
 
 function validateUserDetails()
 {
@@ -19,3 +21,4 @@ function validateUserDetails()
 
 validateUserDetails $firstName $namePattern
 validateUserDetails $lastName $namePattern
+validateUserDetails $userEmail $emailPattern
